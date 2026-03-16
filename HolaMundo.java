7fc7fb69b584c9ef8d807df98c57e98f3f21b6bf
Paula Alonso <mjsafr951753@gmail.com>
2026-03-16 11:54:55 +0100

    Hola mundo

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    /** @return devuelve !Hola,Mundo!
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo!");
+    }
+}
